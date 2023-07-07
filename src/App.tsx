6c2ea256829f7d742e564a277a88f1897ab45eca
siwooJang <joy8417@naver.com>
2023-07-07 23:55:18 +0900

    add func

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import './App.css';
 import Store from './Store';
-import {Restaurant} from "./model/restaurant"
+import {Address,Restaurant} from "./model/restaurant"
 
 
 let data:Restaurant = {
@@ -14,8 +14,12 @@ let data:Restaurant = {
   },
   menu:[{name:"pasta",price:2000,category:"PASTA"},{name:"steak",price:5000,category:"MEAT"}]
 }
+
 const App:React.FC = () => {
   const [myRestaurant,setMyRestaurant] = useState<Restaurant>(data)
+  const changeAddress = (address:Address) => {
+    setMyRestaurant({...myRestaurant,address:address})
+  }
   return (
     <div className="App">
       <Store info={myRestaurant}/>
